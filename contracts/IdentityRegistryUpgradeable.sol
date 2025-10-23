60494e85bca27ecd176083c1702eddf63c16d470
zfdang <zfdang@sparsity.xyz>
2025-10-23 03:52:47 +0000

    Add Methods to Retrieve All Metadata Keys and Entries

--- contracts/IdentityRegistryUpgradeable.sol
+++ contracts/IdentityRegistryUpgradeable.sol
@@ -17,6 +17,12 @@ contract IdentityRegistryUpgradeable is
     // agentId => key => value
     mapping(uint256 => mapping(string => bytes)) private _metadata;
 
+    // agentId => array of keys
+    mapping(uint256 => string[]) private _metadataKeys;
+
+    // agentId => key => index in _metadataKeys array (plus 1, 0 means not exists)
+    mapping(uint256 => mapping(string => uint256)) private _metadataKeyIndex;
+
     struct MetadataEntry {
         string key;
         bytes value;
@@ -59,9 +65,18 @@ contract IdentityRegistryUpgradeable is
         emit Registered(agentId, tokenUri, msg.sender);
 
         for (uint256 i = 0; i < metadata.length; i++) {
-            _metadata[agentId][metadata[i].key] = metadata[i].value;
+            _setMetadataInternal(agentId, metadata[i].key, metadata[i].value);
             emit MetadataSet(agentId, metadata[i].key, metadata[i].key, metadata[i].value);
         }
+    }
+
+    function _setMetadataInternal(uint256 agentId, string memory key, bytes memory value) private {
+        // If key doesn't exist yet, add it to the keys array
+        if (_metadataKeyIndex[agentId][key] == 0) {
+            _metadataKeys[agentId].push(key);
+            _metadataKeyIndex[agentId][key] = _metadataKeys[agentId].length;
+        }
+        _metadata[agentId][key] = value;
     }
 
     function getMetadata(uint256 agentId, string memory key) external view returns (bytes memory) {
@@ -75,8 +90,26 @@ contract IdentityRegistryUpgradeable is
             msg.sender == getApproved(agentId),
             "Not authorized"
         );
-        _metadata[agentId][key] = value;
+        _setMetadataInternal(agentId, key, value);
         emit MetadataSet(agentId, key, key, value);
+    }
+
+    function getAllMetadataKeys(uint256 agentId) external view returns (string[] memory) {
+        return _metadataKeys[agentId];
+    }
+
+    function getAllMetadata(uint256 agentId) external view returns (MetadataEntry[] memory) {
+        string[] memory keys = _metadataKeys[agentId];
+        MetadataEntry[] memory entries = new MetadataEntry[](keys.length);
+
+        for (uint256 i = 0; i < keys.length; i++) {
+            entries[i] = MetadataEntry({
+                key: keys[i],
+                value: _metadata[agentId][keys[i]]
+            });
+        }
+
+        return entries;
     }
 
     function setAgentUri(uint256 agentId, string calldata newUri) external {
